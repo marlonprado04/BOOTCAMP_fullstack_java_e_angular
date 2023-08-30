52701d0d58477bf9dbab0405c3860a7d488e0158
Marlon Prado <marlonprado04@gmail.com>
2023-08-30 14:51:31 -0300

    feat: add class 05 -> function

---
+++ 06_CURSO_sintaxe_basica_javascript/05_aula_funcoes/script.js
@@ -0,0 +1,17 @@
+// Função de exemplo
+function funcao (){
+    console.log("Olá galera");
+}
+
+funcao();
+funcao();
+
+// --------------------------
+
+// Função com parâmetro
+function imprimeMensagem (mensagem){
+    console.log(mensagem);
+}
+
+imprimeMensagem("Olá galerinha do pião");
+imprimeMensagem("Como estão?")
