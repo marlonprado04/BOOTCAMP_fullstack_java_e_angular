50057b938e7e693377c5b2c68c0b96457aed845e
Marlon Prado <marlonprado04@gmail.com>
2023-09-07 21:19:47 -0300

    feat: add fetch example

--- 09_CURSO_dominando_http_e_integrando_pokeapi/assets/javascript/main.js
+++ 09_CURSO_dominando_http_e_integrando_pokeapi/assets/javascript/main.js
@@ -1,1 +1,14 @@
-console.log("Olá galerinha");
+
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+// Fetch nos retorna uma promise 
+// Que é a promessa de um resultado assincrono de uma busca na API
+// Fetch usa o método then para trabalhar com o resultado da consulta, que recebe uma função callback
+fetch(url).then(function (response){
+    console.log(response);
+});
+
+const x = 10 + 10;
+console.log(x);
