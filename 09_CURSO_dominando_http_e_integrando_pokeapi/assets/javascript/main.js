ac0bba5ac0f3a68fa73d1763cb415e9f7e1127b8
Marlon Prado <marlonprado04@gmail.com>
2023-09-07 21:57:11 -0300

    feat: add aula

--- 09_CURSO_dominando_http_e_integrando_pokeapi/assets/javascript/main.js
+++ 09_CURSO_dominando_http_e_integrando_pokeapi/assets/javascript/main.js
@@ -1,14 +1,27 @@
-
 const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-// Fetch nos retorna uma promise 
+// Fetch nos retorna uma promise
 // Que é a promessa de um resultado assincrono de uma busca na API
-// Fetch usa o método then para trabalhar com o resultado da consulta, que recebe uma função callback
-fetch(url).then(function (response){
-    console.log(response);
-});
+fetch(url)
+    // then serve para trabalhar com uma consulta bem sucedida com a api
+    .then(function (response) {
+        // Convertendo a resposta em json, que retorna uma promise onde podemos usar o padrão then / catch / finnaly
+        return response.json()
+    })
 
-const x = 10 + 10;
-console.log(x);
+    // Trabalhando com o retorno da função acima (promise da conversão)
+    .then(function (jsonbody){
+        console.log(jsonbody);
+    })
+
+    //catch serve para trabalhar com uma consulta mal sucedida com a api
+    .catch(function (erro){ //
+        console.log(erro);
+    })
+
+    // finally serve para trabalhar com a consulta independente do sucesso dela ou não
+    .finally(function(){
+        console.log("Requisição concluída!")
+    })
