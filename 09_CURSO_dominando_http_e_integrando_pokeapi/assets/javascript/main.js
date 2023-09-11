84223eb1a791b358320859856cc71ab1b4e12491
Marlon Prado <marlonprado04@gmail.com>
2023-09-11 14:43:08 -0300

    feat: add arrow functions

--- 09_CURSO_dominando_http_e_integrando_pokeapi/assets/javascript/main.js
+++ 09_CURSO_dominando_http_e_integrando_pokeapi/assets/javascript/main.js
@@ -6,22 +6,10 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 // Que é a promessa de um resultado assincrono de uma busca na API
 fetch(url)
     // then serve para trabalhar com uma consulta bem sucedida com a api
-    .then(function (response) {
-        // Convertendo a resposta em json, que retorna uma promise onde podemos usar o padrão then / catch / finnaly
-        return response.json()
-    })
+    .then((response) => response.json())
 
-    // Trabalhando com o retorno da função acima (promise da conversão)
-    .then(function (jsonbody){
-        console.log(jsonbody);
-    })
+    // Trabalhando com o retorno da função acima (que é um promise da conversão, já que pode não funcionar)
+    .then((jsonbody) => console.log(jsonbody))
 
-    //catch serve para trabalhar com uma consulta mal sucedida com a api
-    .catch(function (erro){ //
-        console.log(erro);
-    })
-
-    // finally serve para trabalhar com a consulta independente do sucesso dela ou não
-    .finally(function(){
-        console.log("Requisição concluída!")
-    })
+    //catch serve para trabalhar com a consulta mal sucedida
+    .catch((erro) => console.log(erro))
