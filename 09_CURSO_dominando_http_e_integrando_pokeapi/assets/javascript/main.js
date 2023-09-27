d06573a462687ea34b6c0a599085c97fcb274148
Marlon Prado <marlonprado04@gmail.com>
2023-09-27 15:24:20 -0300

    feat: add code for pagination dynamic

--- 09_CURSO_dominando_http_e_integrando_pokeapi/assets/javascript/main.js
+++ 09_CURSO_dominando_http_e_integrando_pokeapi/assets/javascript/main.js
@@ -1,14 +1,29 @@
 const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
+const limit = 5;
+let offset = 0;
 
-// Função para converter resultado de API para lista do HTML
-function convertPokemonToLi(pokemon) {
-  return `<li class="pokemon ${pokemon.type}">
+function loadMoreItems(offset, limit) {
+  // Chamando requisição de API e listando pokemon no browse
+  pokeApi
+    .getPokemons(offset, limit)
+    .then((pokemons = []) => {
+      // Primeiro mapeia todos os pokemons e os converte em li de HTML via MAP e convertPokemonToLi
+      // Depois unifica tudo em uma string sem separador via JOIN
+      // Depois incrementa lista convertida em string no html via innerHTML
+      const newHtml = pokemons
+        .map((pokemon) => {
+          return `<li class="pokemon ${pokemon.type}">
                 <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
 
                 <div class="detail">
                     <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type ${type}"> ${type}</li>`).join("")}
+                        ${pokemon.types
+                          .map(
+                            (type) => `<li class="type ${type}"> ${type}</li>`
+                          )
+                          .join("")}
                     </ol>
 
                     <img src="${pokemon.photo}"
@@ -16,17 +31,17 @@ function convertPokemonToLi(pokemon) {
                 </div>
 
             </li>`;
+        })
+        .join("");
+      pokemonList.innerHTML += newHtml;
+    })
+    // Retorna erro caso promise dê errado
+    .catch((erro) => console.log(erro));
 }
 
-// Chamando requisição de API e listando pokemon no browse
-pokeApi
-  .getPokemons()
-  .then((pokemons = []) => {
-    // Primeiro mapeia todos os pokemons e os converte em li de HTML via MAP e convertPokemonToLi
-    // Depois unifica tudo em uma string sem separador via JOIN
-    // Depois incrementa lista convertida em string no html via innerHTML
-    const newHtml = pokemons.map(convertPokemonToLi).join("");
-    pokemonList.innerHTML = newHtml;
-  })
-  // Retorna erro caso promise dê errado
-  .catch((erro) => console.log(erro));
+loadMoreItems(offset, limit);
+
+loadMoreButton.addEventListener("click", () => {
+  offset += limit;
+  loadMoreItems(offset, limit);
+});
