3508d37552791de3fa0bee0f746215c30d1e4cda
Marlon Prado <marlonprado04@gmail.com>
2023-09-28 23:34:14 -0300

    feat: add TituloDinamico

---
+++ 17_CURSO_web_components/src/Components/TituloDinamico.js
@@ -0,0 +1,25 @@
+class TituloDinamico extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+
+    // Cria estrutura
+    const componentRoot = document.createElement("h1");
+    componentRoot.textContent = "Marlon";
+
+    // Estiliza componente
+    const style = document.createElement("style");
+    style.textContent = `
+        h1{
+            color: red;
+        }
+    `;
+
+    // Adiciona à shadow
+    shadow.appendChild(componentRoot);
+    shadow.appendChild(style);
+  }
+}
+
+// Atribui seletor
+customElements.define("titulo-dinamico", TituloDinamico);
