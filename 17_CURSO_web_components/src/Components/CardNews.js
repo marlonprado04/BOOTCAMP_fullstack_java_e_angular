b9a61741ea0c88ee1b46ad0fea9e34ea6b192661
Marlon Prado <marlonprado04@gmail.com>
2023-09-28 23:59:54 -0300

    feat: add base of CardNews

--- 17_CURSO_web_components/src/Components/CardNews.js
+++ 17_CURSO_web_components/src/Components/CardNews.js
@@ -1,17 +1,38 @@
 class CardNews extends HTMLElement {
-  // Criando construtor da classe atual (CardNews)
   constructor() {
-    // Chamando construtor da classe pai (HTMLElement)
     super();
 
-    // Cria uma shadow dom (attachShadow) a partir dessa classe (this) 
-    // no modo open, ou seja, ela pode manipular e ser manipulada pela DOM do navegador 
-    const shadow = this.attachShadow({mode: "open"});
-    
-    // Incrementa no HTML uma tag h1 sempre que chamada
-    shadow.innerHTML = "<h1>Hello World</h1>";
+    // Cria shadow de forma aberta
+    const shadow = this.attachShadow({ mode: "open" });
+
+    // Pendurando filhos na shadow DOM de acordo com métodos
+    shadow.appendChild(this.build());
+    shadow.appendChild(this.styles());
   }
+
+  // Método para construir o HTML
+  build() {
+    // Constrói elemento pai (raiz do esqueleto)
+    const componentRoot = document.createElement("div");
+    // Setta atributo class=card ao elemento pai
+    componentRoot.setAttribute("class", "card");
+
+    // Constrói elementos filhos e suas classes
+    const cardLeft = document.createElement("div");
+    cardLeft.setAttribute("class", "card___left");
+    const cardRight = document.createElement("div");
+    cardRight.setAttribute("class", "card__right");
+
+    // Adiciona filhos à estrutura 
+    componentRoot.appendChild(cardLeft);
+    componentRoot.appendChild(cardRight);
+
+    // Retorna elemento pai e por consequência seus filhos
+    return componentRoot;
+  }
+
+  // Método para construir o CSS
+  styles() {}
 }
 
-//  Define um termo customizado para a tag, nesse caso, card-news
-customElements.define("card-news", CardNews)
+customElements.define("card-news", CardNews);
