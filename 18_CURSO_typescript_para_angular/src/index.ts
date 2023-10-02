a063a5a460f1823ab5ebff4c147480ac94b660d4
Marlon Prado <marlonprado04@gmail.com>
2023-10-02 19:22:39 -0300

    feat: add generic types example

--- 18_CURSO_typescript_para_angular/src/index.ts
+++ 18_CURSO_typescript_para_angular/src/index.ts
@@ -1,19 +1,19 @@
 // Generics
 
-// Create function to receive and concatenate any types of array and return any types of array
-function concatArray(...itens:any[]): any[]{
+// Create function to receive and concatenate generic array information and return a generic array information
+// T on uppercase represents a generic information
+function concatArray<T>(...itens:T[]): T[]{
     return new Array().concat(...itens);
 }
 
 // Create new arrays to string and number
-const numArray = concatArray([1,5], [3]);
-const stringArray = concatArray(["Marlon", "João"], ["Goku"]);
+const numArray = concatArray<number[]>([1,5], [3]);
+const stringArray = concatArray<string[]>(["Marlon", "João"], ["Goku"]);
 
 // Print created arrays
 console.log(numArray);
 console.log(stringArray);
 
-// This code has a problema:
-// When creating an array of number, it allows incrementing with string
-numArray.push("Marlon");
+// When creating an array of number, it not allow incrementing with string, because was delimited of generics
+// numArray.push("Marlon");
 
