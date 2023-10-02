e00ba9fd24ec7cd60544f868f0270a513e17a176
Marlon Prado <marlonprado04@gmail.com>
2023-10-02 19:02:39 -0300

    feat: add subclass example

--- 18_CURSO_typescript_para_angular/build/index.js
+++ 18_CURSO_typescript_para_angular/build/index.js
@@ -7,7 +7,7 @@ class Character {
         this.stregth = stregth;
         this.skill = skill;
     }
-    // Methor to attack
+    // Method to attack
     attack() {
         console.log(`Attack with ${this.stregth} points!`);
     }
@@ -18,3 +18,22 @@ const c1 = new Character("Marlon", 10, 4);
 console.log(c1);
 // Print character attack
 c1.attack();
+//----------------------------------------------
+// Subclass
+// On creation of subclass:
+// Character: superclass
+// Magician: subclass
+class Magician extends Character {
+    constructor(name, stregth, skill, magicPoints) {
+        // Calling super to invoke the parent class's constructor and pass values 
+        super(name, stregth, skill);
+        // Pass magicpoint to attribute of class
+        this.magicPoints = magicPoints;
+        // Trying acess parents class attributes according with data modifiers
+        //this.name; // Dont work because is private
+        this.stregth; // Its work because is protected
+        this.skill; // Its work because is protected
+    }
+}
+// Create new character magician
+const c2 = new Magician("Gandalf", 110, 60, 80);
--- 18_CURSO_typescript_para_angular/src/index.ts
+++ 18_CURSO_typescript_para_angular/src/index.ts
@@ -1,15 +1,15 @@
 // Classes
 
 class Character {
-    name: string;
-    stregth: number;
+    private name: string;
+    protected stregth: number;
     skill: number;
 
     // Passing age as optional information
-    age?:number
+    age?: number
 
     // Constructor for create a class
-    constructor(name: string, stregth: number, skill: number){
+    constructor(name: string, stregth: number, skill: number) {
         this.name = name;
         this.stregth = stregth;
         this.skill = skill;
@@ -29,3 +29,31 @@ console.log(c1);
 
 // Print character attack
 c1.attack();
+
+
+//----------------------------------------------
+// Subclass
+// On creation of subclass:
+// Character: superclass
+// Magician: subclass
+class Magician extends Character {
+    // Add new attribute
+    magicPoints: number;
+
+    constructor(name: string, stregth: number, skill: number, magicPoints: number) {
+        // Calling super to invoke the parent class's constructor and pass values 
+        super(name, stregth, skill);
+
+        // Pass magicpoint to attribute of class
+        this.magicPoints = magicPoints;
+
+        // Trying acess parents class attributes according with data modifiers
+        //this.name; // Dont work because is private
+        this.stregth // Its work because is protected
+        this.skill // Its work because is protected
+    }
+}
+
+// Create new character magician
+const c2 = new Magician("Gandalf", 110, 60, 80);
+
