538b324b7d8bb09e841dc2c127d7c3f3d7f2e921
Marlon Prado <marlonprado04@gmail.com>
2023-10-02 17:09:16 -0300

    feat: add interface and class examples

--- 18_CURSO_typescript_para_angular/build/index.js
+++ 18_CURSO_typescript_para_angular/build/index.js
@@ -1,1 +1,30 @@
 "use strict";
+//Interfaces são como types
+// Criando um robo usando o type
+let robo = {
+    name: "Megamen",
+    id: 1
+};
+// Criando um robo a partir da interface
+let robo2 = {
+    id: 1,
+    name: "Pluto",
+    sayHello: function (name) {
+        return `Hello ${name}`;
+    }
+};
+// Criando classe pessoa a partir da interface robot2
+class Pessoa {
+    // Criando método construtor da classe pessoa
+    constructor(id, name) {
+        this.name = name;
+        this.id = id;
+    }
+    // Criando método exigido pela interface
+    sayHello() {
+        return `Hello ${this.name}`;
+    }
+}
+// Criando pessoa nova
+const p = new Pessoa(1, "Marlon");
+console.log(p.sayHello());
