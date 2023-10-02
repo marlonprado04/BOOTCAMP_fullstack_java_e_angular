5ba45b88073462c56dcf28844e886f62075977f5
Marlon Prado <marlonprado04@gmail.com>
2023-10-02 19:58:47 -0300

    feat: add decorator example

--- 18_CURSO_typescript_para_angular/src/index.ts
+++ 18_CURSO_typescript_para_angular/src/index.ts
@@ -1,9 +1,18 @@
 // decorators
 
-function ExibirNome(target:any){
-    console.log(target);
+// Injeta a versão da API dinâmicamente quando chamado
+function apiVersion (version: string){
+    return (target: any) => { 
+        Object.assign(target.prototype, {__version: version})
+    }
 }
 
-@ExibirNome
-class Funcionario{
-}
+// Chamando decorator acima da classe Api
+@apiVersion("1.10")
+class Api{}
+
+// Criando nova API
+const api = new Api();
+
+// Imprimindo propriedade injetada via decorator
+console.log(api.__version);
