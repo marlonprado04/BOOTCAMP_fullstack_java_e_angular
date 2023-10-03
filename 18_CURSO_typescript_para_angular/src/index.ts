19aa1f8a6340a98b7165401840144c3dc96a1f5f
Marlon Prado <marlonprado04@gmail.com>
2023-10-02 22:02:43 -0300

    feat: add  attribute decorators

--- 18_CURSO_typescript_para_angular/src/index.ts
+++ 18_CURSO_typescript_para_angular/src/index.ts
@@ -1,25 +1,41 @@
 // decorators
 
-// Injeta a versão da API dinâmicamente quando chamado
-function apiVersion (version: string){
-    return (target: any) => { 
-        Object.assign(target.prototype, {__version: version})
-    }
-}
+// Função decorator minLength que verifica o comprimento mínimo da propriedade
+function minLength(length: number) {
+    return (target: any, key: string) => {
+        // Armazena o valor original da propriedade em _value
+        let _value = target[key];
 
-function minLength(length:number){
-    return (target:any, key:string) => {
-        console.log(target[key]);
-    }
-}
+        // Define um getter personalizado que retorna o valor armazenado em _value
+        const getter = () => _value;
 
-// Chamando decorator acima da classe Api
-@apiVersion("1.10")
-class Api{
+        // Define um setter personalizado que verifica o comprimento mínimo
+        const setter = (value: string) => {
+            if (value.length < length) {
+                // Lança um erro se o comprimento do valor for menor que o especificado
+                throw new Error(`Tamanho menor do que ${length}.`);
+            } else {
+                // Caso contrário, atualiza o valor em _value
+                _value = value;
+            };
+        };
+
+        // Substitui a definição da propriedade original com getter e setter personalizados
+        Object.defineProperty(target, key, {
+            get: getter,
+            set: setter
+        });
+    };
+};
+
+// Aplicando o decorator apiVersion à classe Api
+class Api {
+    // Aplicando o decorator minLength à propriedade name da classe Api
     @minLength(3)
-    name:string;
+    name: string;
 
-    constructor(name:string){
+    constructor(name: string) {
+        // Inicializa a propriedade name com o valor passado no construtor
         this.name = name;
     }
 }
@@ -27,5 +43,4 @@ class Api{
 // Criando nova API
 const api = new Api("Produtos");
 
-// Imprimindo propriedade injetada via decorator
 console.log(api.name);
