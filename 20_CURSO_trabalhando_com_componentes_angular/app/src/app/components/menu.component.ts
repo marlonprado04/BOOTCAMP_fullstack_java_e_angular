089143bb8fe41df233dabef75e7e0347c1fe0635
Marlon Prado <marlonprado04@gmail.com>
2023-10-08 17:12:41 -0300

    feat: add initial files to angular project

---
+++ 20_CURSO_trabalhando_com_componentes_angular/app/src/app/components/menu.component.ts
@@ -0,0 +1,15 @@
+// Importando component como contrato do angular core
+import { Component } from '@angular/core';
+
+// Criando decorator para criar component
+@Component({
+    // Declarando seletor para uso no HTML
+    selector: 'menu-component',
+    // Declarando template do seletor
+    template: '<h2> Marlon </h2>',
+    // Declarando estilo do seletor
+    styles: ['h2{color:blue}'],
+})
+
+// Exportando clsse após criar o component
+export class MenuComponent { }
