89f7f115c0be9bbf6e725e87a6db1cf0273f3a09
Marlon Prado <marlonprado04@gmail.com>
2023-10-09 20:05:45 -0300

    feat: add docheck examples

--- 21_CURSO_lifecycle_hooks_angular/life_cycle/src/app/check-sample/check-sample.component.ts
+++ 21_CURSO_lifecycle_hooks_angular/life_cycle/src/app/check-sample/check-sample.component.ts
@@ -1,42 +1,68 @@
-import { Component,
+import {
+	Component,
 	OnInit,
 	DoCheck,
 	AfterContentChecked,
 	AfterContentInit,
 	AfterViewChecked,
-	AfterViewInit} from '@angular/core';
+	AfterViewInit
+} from '@angular/core';
 
 @Component({
-  selector: 'app-check-sample',
-  templateUrl: './check-sample.component.html',
-  styleUrls: ['./check-sample.component.css']
+	selector: 'app-check-sample',
+	templateUrl: './check-sample.component.html',
+	styleUrls: ['./check-sample.component.css']
 })
 export class CheckSampleComponent implements OnInit, DoCheck,
-AfterContentChecked,
-AfterContentInit,
-AfterViewChecked,
-AfterViewInit {
+	AfterContentChecked,
+	AfterContentInit,
+	AfterViewChecked,
+	AfterViewInit {
 
-  constructor() { }
+	quantidade: number = 0;
+
+	constructor() { }
+
+	adicionar() {
+		this.quantidade += 1;
+	}
+
+	subtrair() {
+		this.quantidade -= 1;
+	}
+
+
+	// checked -> content -> view
+
+
+	// Faz a checagem
 	ngDoCheck(): void {
 		console.log(`ngDoCheck`)
 	}
-	ngAfterContentChecked(): void {
-		console.log(`ngAfterContentChecked`)
-	}
+
+	// Inicia conteúdo
 	ngAfterContentInit(): void {
 		console.log(`ngAfterContentInit`)
 	}
-	ngAfterViewChecked(): void {
-		console.log(`ngAfterViewChecked`)
-	}
+
+	// Inicia view após conteúdo
 	ngAfterViewInit(): void {
 		console.log(`ngAfterViewInit`)
 	}
 
-  ngOnInit(): void {
+	// Após alteração, verifica o conteúdo
+	ngAfterContentChecked(): void {
+		console.log(`ngAfterContentChecked`)
+	}
+
+	// Após verificar conteúdo, verifica view
+	ngAfterViewChecked(): void {
+		console.log(`ngAfterViewChecked`)
+	}
+
+	ngOnInit(): void {
 		console.log(`ngOnInit`)
 
-  }
+	}
 
 }
