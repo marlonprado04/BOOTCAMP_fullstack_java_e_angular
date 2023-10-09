4ad550caff04d56e1d4c875cc3473f4ea48bc1bd
Marlon Prado <marlonprado04@gmail.com>
2023-10-09 19:40:42 -0300

    feat: add examples of ngOnChanges and ngOnInit

--- 21_CURSO_lifecycle_hooks_angular/life_cycle/src/app/title/title.component.ts
+++ 21_CURSO_lifecycle_hooks_angular/life_cycle/src/app/title/title.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input} from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent implements OnInit{
+export class TitleComponent implements OnInit, OnChanges{
 
-  constructor() { }
+	// Inputando valor do nome
+	@Input() nome:string = "";
+
+  constructor() {
+		console.log(`Construtor ${this.nome}`);
+	}
 
 	// Método OnInit que executa na criação do component
 	ngOnInit(): void {
-		console.log("hello hello")
+		console.log(`OnInit ${this.nome}`);
 	}
 
+	// Método OnChanges qie executa nas mudanças
+	// É uma interface sendo implementada na classe
+	ngOnChanges(): void {
+		console.log(`OnChange ${this.nome}`);
+	}
 
 }
