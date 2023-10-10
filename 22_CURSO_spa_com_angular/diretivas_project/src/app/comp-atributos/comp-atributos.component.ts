f585557a0b6abf6f02461288143fff14183967f2
Marlon Prado <marlonprado04@gmail.com>
2023-10-10 20:34:03 -0300

    feat: add ngClass example

---
+++ 22_CURSO_spa_com_angular/diretivas_project/src/app/comp-atributos/comp-atributos.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-atributos',
+  templateUrl: './comp-atributos.component.html',
+  styleUrls: ['./comp-atributos.component.css']
+})
+export class CompAtributosComponent implements OnInit {
+
+  estilo:string="enable";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  trocar(){
+    if(this.estilo === "disable"){
+      this.estilo = "enable";
+    }else{
+      this.estilo = "disable";
+    }
+  }
+
+}
