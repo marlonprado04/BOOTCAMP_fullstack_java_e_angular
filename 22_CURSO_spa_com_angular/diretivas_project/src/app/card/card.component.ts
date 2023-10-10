e9e37690e33b95dc9999b21df1583a6c685314ba
Marlon Prado <marlonprado04@gmail.com>
2023-10-10 19:47:33 -0300

    feat: add nfFor and ngTemplate example

--- 22_CURSO_spa_com_angular/diretivas_project/src/app/card/card.component.ts
+++ 22_CURSO_spa_com_angular/diretivas_project/src/app/card/card.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  produtos: string[] = [];
+
+  constructor() {
+    this.produtos = ["banana",
+      "maça",
+      "laranja"]
+  }
 
   ngOnInit(): void {
   }
