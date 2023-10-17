8a66a0d356d51a046e19fca11c8f10bdd866cfb3
Marlon Prado <marlonprado04@gmail.com>
2023-10-17 17:36:26 -0300

    feat: add HttpClient to get pokemon info

--- 26_CURSO_services_no_angular/angular_services/src/app/services/pokemon.service.ts
+++ 26_CURSO_services_no_angular/angular_services/src/app/services/pokemon.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment'
 
@@ -7,13 +8,22 @@ import { environment } from 'src/environments/environment'
 export class PokemonService {
   // Cria variável para armazenar URL da pokeApi
   private baseURL: string = "";
+  // Cria variável para armazenar dados do Pokémon saídos da requisição
+  private pokeData: any;
 
-  constructor() {
-    // Ao construir o service passa a URL do environment para a variável do service
-    this.baseURL = environment.pokeApi
+  constructor(
+    private http: HttpClient
+  ) {
+    // Ao construir o serviço, passa a URL do ambiente (environment) para a variável do serviço
+    this.baseURL = environment.pokeApi;
   }
 
   getPokemon(pokemonName: string) {
-    console.log(this.baseURL);
+    // Realiza uma requisição HTTP GET para buscar dados de um Pokémon
+    // A URL é construída combinando a URL base com o nome do Pokémon fornecido como argumento
+    this.pokeData = this.http.get(`${this.baseURL}${pokemonName}`);
+
+    // Exibe os dados do Pokémon no console (pode ser removido em produção)
+    console.log(this.pokeData);
   }
 }
