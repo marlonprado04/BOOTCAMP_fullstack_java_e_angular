00d191655b0bb556f1307278a24fa4cd842f3c21
Marlon Prado <marlonprado04@gmail.com>
2023-10-17 17:17:46 -0300

    feat: add service communication to enviroment

--- 26_CURSO_services_no_angular/angular_services/src/app/services/pokemon.service.ts
+++ 26_CURSO_services_no_angular/angular_services/src/app/services/pokemon.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
+  // Cria variável para armazenar URL da pokeApi
+  private baseURL: string = "";
 
-  constructor() { }
+  constructor() {
+    // Ao construir o service passa a URL do environment para a variável do service
+    this.baseURL = environment.pokeApi
+  }
 
-  getPokemon(pokemonName:string){
-    console.log(pokemonName);
+  getPokemon(pokemonName: string) {
+    console.log(this.baseURL);
   }
 }
