252dd7ca2ba54652f6cbf736bd2d19fb6be7740a
Marlon Prado <marlonprado04@gmail.com>
2023-10-17 14:40:23 -0300

    feat: add activatedRoute examples

--- 25_CURSO_rotas_no_angular/angular_routes/src/app/pages/portfolio/card/card.component.ts
+++ 25_CURSO_rotas_no_angular/angular_routes/src/app/pages/portfolio/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activeRoute: ActivatedRoute) {
+
+    // Recupera id: http://localhost:4200/portfolio/1
+    this.activeRoute.params.subscribe(
+      res => console.log(res)
+    )
+
+    // Recupera QueryParams: http://localhost:4200/portfolio/1?name=marlon&idade=23
+    this.activeRoute.queryParams.subscribe(
+      res => console.log(res)
+    )
+  }
 
   ngOnInit(): void {
   }
