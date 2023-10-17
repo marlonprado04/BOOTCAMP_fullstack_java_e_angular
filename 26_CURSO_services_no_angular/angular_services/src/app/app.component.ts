4154999a92c66e2059228e5d2f815681cf2e5bf4
Marlon Prado <marlonprado04@gmail.com>
2023-10-17 16:25:49 -0300

    feat: add card component

--- 26_CURSO_services_no_angular/angular_services/src/app/app.component.ts
+++ 26_CURSO_services_no_angular/angular_services/src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>Aula de services</h1>
+  <app-card></app-card>
+  <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
