996122b32f9b5f5d308c0bc7085944449276ea10
Marlon Prado <marlonprado04@gmail.com>
2023-10-17 01:11:58 -0300

    feat: add routes example

--- 25_CURSO_rotas_no_angular/angular_routes/src/app/app-routing.module.ts
+++ 25_CURSO_rotas_no_angular/angular_routes/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portfolio/card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: TitleComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:"portfolio",
+    component:CardComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
