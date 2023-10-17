98235ce8e9a366e507076a5ff010df5a5d0fe1cf
Marlon Prado <marlonprado04@gmail.com>
2023-10-17 01:30:55 -0300

    feat: add routerLink

--- 25_CURSO_rotas_no_angular/angular_routes/src/app/app.module.ts
+++ 25_CURSO_rotas_no_angular/angular_routes/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {IndexModule} from './pages/index/index.module'
-import {PortfolioModule} from './pages/portfolio/portfolio.module'
+import {PortfolioModule} from './pages/portfolio/portfolio.module';
+import { MenuComponent } from './shared/menu/menu.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
