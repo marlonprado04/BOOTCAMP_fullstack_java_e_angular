c9889af938fce69bcae0224d25044d2799eed3aa
Marlon Prado <marlonprado04@gmail.com>
2023-10-17 23:46:47 -0300

    feat: add pokemonData structure on card component and html

--- 26_CURSO_services_no_angular/angular_services/src/app/components/card/card.component.ts
+++ 26_CURSO_services_no_angular/angular_services/src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
 import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
@@ -7,6 +8,8 @@ import { PokemonService } from 'src/app/services/pokemon.service';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  // Criando variável pokemon do tipo PokemonData (com toda estrutura de um pokemon)
+  pokemon: PokemonData | any;
 
   // Criando propriedades do pokemon
   name: string = "CHARIZARD"
@@ -19,7 +22,16 @@ export class CardComponent implements OnInit {
   ngOnInit(): void {
     this.service.getPokemon("charizard").subscribe(
       {
-        next: (res) => console.log(res),
+        next: (res) => {
+          // Passando as informações da consulta da API para a variável pokemon criada nesse componente
+          this.pokemon = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types
+          }
+
+        },
         error: (err) => console.log(err)
       }
     );
--- 26_CURSO_services_no_angular/angular_services/src/app/models/pokemonData.ts
+++ 26_CURSO_services_no_angular/angular_services/src/app/models/pokemonData.ts
@@ -1,4 +1,8 @@
 export type PokemonData = {
-  name:string;
-  id:number;
+  name: string;
+  id: number;
+  sprites: {
+    front_default: string;
+  };
+  types:[];
 }
