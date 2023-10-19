31fe02082ab9a87aeb0d40aa3d5ec0e760a1d2ac
Marlon Prado <marlonprado04@gmail.com>
2023-10-19 15:58:48 -0300

    feat: add increment example

--- 30_CURSO_sintaxe_java/tipos_e_variaveis/src/Operadores.java
+++ 30_CURSO_sintaxe_java/tipos_e_variaveis/src/Operadores.java
@@ -1,29 +1,45 @@
 public class Operadores {
 
-  public static void main (String[] args){
+  public static void main(String[] args) {
 
     int numero = 5;
 
     // Número fica negativo na impressão
-    System.out.println(- numero); 
+    System.out.println(-numero);
 
     // Número volta ao seu valor original
-    System.out.println(numero); 
+    System.out.println(numero);
 
     // Mudando o valor para o valor dele mesmo, negativo
-    numero = - numero; 
-    
+    numero = -numero;
+
     // Número passou a ser negativo definitivamente
     System.out.println(numero);
 
     // Isso não faz o número ficar positivo, pois esse símbolo significa soma
-    numero = + numero;
+    numero = +numero;
     System.out.println(numero);
 
     // Para mudar o número para positivo, precisamos multiplicar por -1
 
     numero = numero * -1;
     System.out.println(numero);
+
+    // Incremento
+    System.out.println(numero); // 5
+    numero++; //6
+    System.out.println(numero); //6
+
+    // Decremento
+    System.out.println(numero); //6
+    numero--; //5
+    System.out.println(numero);//5
+
+    // Inversão / negação de boleano
+    boolean boleana = true;
+    System.out.println(boleana); // true
+    System.out.println(!boleana); // false
+
   }
-  
+
 }
