10bd011e48e43f8bec9081b48771d1ddb119a400
Marlon Prado <marlonprado04@gmail.com>
2023-10-19 00:19:14 -0300

    feat: add firs method example

--- 30_CURSO_sintaxe_java/anatomia_das_classes/src/MinhaClasse.java
+++ 30_CURSO_sintaxe_java/anatomia_das_classes/src/MinhaClasse.java
@@ -1,5 +1,14 @@
 public class MinhaClasse {
   public static void main(String[] args) {
-    System.out.print("Olá mundo!");
+    // Chamando método para juntar nomes e atribuindo à variável nome
+    String nome = nomeCompleto("Marlon", "Prado");
+
+    // Imprimindo variável nome
+    System.out.println(nome);
+  }
+
+  // Método para juntar nomes
+  public static String nomeCompleto(String primeiroNome, String segundoNome) {
+    return primeiroNome.concat(" ").concat(segundoNome);
   }
 }
